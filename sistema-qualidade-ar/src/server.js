0c1688573a3230cde60695f4f61d05ea5b86a099
joicy kelly <joicykelly2301@gmail.com>
2024-09-21 02:52:14 -0300

    ➕ :heavy_plus_sign: installation CORS

--- sistema-qualidade-ar/src/server.js
+++ sistema-qualidade-ar/src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
+import cors from "cors"; // Importar o CORS
 import { deviceAirRouter } from "./routes/qualityAir.routes.js";
 
 const app = express();
 const port = 3000;
 
+app.use(cors()); // Habilitar CORS
 app.use(express.json());
 app.use(deviceAirRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
